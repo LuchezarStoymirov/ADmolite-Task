df5cea99289e0d623a95b49c7794043f8f80ddb1
LuchezarStoymirov <luchostoimirov@gmail.com>
2023-08-11 09:13:12 +0300

    add styling details

--- ADmolite-Test/src/Components/Header/Header.tsx
+++ ADmolite-Test/src/Components/Header/Header.tsx
@@ -6,6 +6,11 @@ export const Header = () => {
             <div className={style.logo}>
         
             </div>
+            <div className={style.socialMedia}>
+                <img src="src/Assets/facebook.png" alt="facebook" className={style.icon}/>
+                <img src="src/Assets/instagram.png" alt="instagram" className={style.icon}/>
+                <img src="src/Assets/twitter-sign.png" alt="twitter" className={style.icon}/>
+            </div>
         </div>
     );
 }
